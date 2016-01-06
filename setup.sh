5dea02a7b93749be4da87520830e48a1c789142d
Dorwin Shields <kavnar@gmail.com>
2016-01-05 22:12:26 -0600

    add step to compile amplitude during initial setup

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@
 docker-machine create --driver virtualbox default
 docker-machine start default
 eval "$(docker-machine env default)"
+./gradlew clean war
 docker build -f Dockerfile -t amplitude/tomee .
