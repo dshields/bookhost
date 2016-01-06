682bb2a4288c83ba7196dc147cd9b80fc413700b
dshields <dorwins@gmail.com>
2016-01-05 21:44:00 -0600

    clean up after docker is done

--- redeploy.sh
+++ redeploy.sh
@@ -3,4 +3,7 @@
 docker build -f Dockerfile .
 echo '"docker-machine ls" to get ip of the machine running tomee then you can browse to http://<ip>:8888/mp3'
 docker run -it -p 8888:8080 amplitude/tomee
+docker rm -v $(docker ps -a -q -f status=exited)
+docker rmi $(docker images -f "dangling=true" -q)
+
 
