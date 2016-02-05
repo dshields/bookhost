ecaade34b4bd50c7bc6a49ebcfb7d0281d4c5c87
dshields <dorwins@gmail.com>
2016-02-04 20:36:20 -0600

    break redploy into build and run scripts

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-./gradlew clean war
 docker build -f Dockerfile .
 echo '"docker-machine ls" to get ip of the machine running tomee then you can browse to http://<ip>:8888/mp3'
 docker run -it -p 8888:8080 amplitude/tomee
